b85fd66e879b06934a558fbfc6e5a2ec9ef3bed5
williamGMK <137037723+williamGMK@users.noreply.github.com>
2025-11-02 07:14:01 +0200

    3D Model created commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
+import ComputersCanvas from "./canvas/Computers";
 
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-secondary" />
@@ -18,21 +18,22 @@ const Hero = () => {
             Hi, I'm <span className="text-secondary">Glodi</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop full-stack web applications,{" "}
+            full-stack web applications Network System ,{" "}
             <br className="sm:block hidden" />
-            and mobile app.
+            and mobile app Engineer.
           </p>
         </div>
       </div>
+
+      {/* 3D Canvas */}
       <ComputersCanvas />
 
+      {/* Scroll Down Indicator */}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
+              animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
